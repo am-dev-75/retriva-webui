993b25fd6d6e905e777ec70e5ac41de6374977c2
Andrea Marson <am.dev.75@gmail.com>
2026-05-10 10:52:50 +0200

    Add licensing headers.

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2026 Andrea Marson (am.dev.75@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export const CONFIG = {
   GATEWAY_BASE_URL: import.meta.env.VITE_RETRIVA_GATEWAY_BASE_URL || 'http://localhost:8080',
   APP_NAME: import.meta.env.VITE_APP_NAME || 'Retriva',
